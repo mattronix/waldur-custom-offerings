247cc28ceec7b22acf3b98f9693fae3b89661f4f
Matthew Frost <m.frost@mattronix.nl>
2024-02-14 17:13:24 +0100

    added exit codes

--- create_user_over_ssh/storage-user-add-waldur.py
+++ create_user_over_ssh/storage-user-add-waldur.py
@@ -2,6 +2,7 @@ import paramiko
 from io import StringIO
 import json 
 import os
+import sys
 
 def ssh_command_with_key_contents(hostname, port, username, key_contents, passphrase, check_user_command, commands):
     try:
@@ -20,19 +21,19 @@ def ssh_command_with_key_contents(hostname, port, username, key_contents, passph
         # Check if user already exists
         stdin, stdout, stderr = client.exec_command(check_user_command, get_pty=True)
         if stdout.channel.recv_exit_status() == 0:
-            print(f"User {new_user} already exists. Exiting.")
-            return
+            print(f"cannot create user {new_user} as they exist already. Exiting ")
+            sys.exit(1)
 
         # If user does not exist, execute the commands
         for command in commands:
             stdin, stdout, stderr = client.exec_command(command, get_pty=True)
-            print(stdout.read().decode())
 
     except Exception as e:
         print(f"Connection failed: {str(e)}")
     finally:
         key_file_obj.close()
         client.close()
+        sys.exit()
 
 if __name__ == "__main__":
 
