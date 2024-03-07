e52d215b3b15dcae6a84f5a953fbc8b60e344398
Matthew Frost <m.frost@mattronix.nl>
2024-03-07 10:08:48 +0100

    basic create script

--- create_user_over_ssh/storage-user-add-waldur.py
+++ create_user_over_ssh/storage-user-add-waldur.py
@@ -21,16 +21,14 @@ def ssh_command_with_key_contents(hostname, port, username, key_contents, passph
         # Check if user already exists
         stdin, stdout, stderr = client.exec_command(check_user_command, get_pty=True)
         if stdout.channel.recv_exit_status() == 0:
-            print(f"cannot create user {new_user} as they exist already. Exiting ")
             sys.exit(1)
 
         # If user does not exist, execute the commands
         for command in commands:
             stdin, stdout, stderr = client.exec_command(command, get_pty=True)
-            print(stdout.read().decode())
 
     except Exception as e:
-        print(f"Connection failed: {str(e)}")
+        pass
     finally:
         key_file_obj.close()
         client.close()
